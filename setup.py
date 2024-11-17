0dfb82e05202314db1fe17b7ae2efbfdadcf4a08
Tomisin Abiodun <decave.12357@gmail.com>
2024-11-17 10:37:43 +0000

    Add description to setup

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ import os
 from setuptools import setup, find_packages
 
 
-version = os.getenv("PACKAGE_VERSION", "0.1.0")
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+version = os.getenv("PACKAGE_VERSION", "0.0.1")
 
 
 setup(
     name="beadify",
     version=version,
     description="A lightweight, efficient tool tailored for hobbyists looking to deploy multiple applications on a single Ubuntu-based VPS (Virtual Private Server)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Tomisin Abiodun",
     author_email="decave.12357@gmail.com",
+    url="https://github.com/CodeLexis/beadify-cli",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
